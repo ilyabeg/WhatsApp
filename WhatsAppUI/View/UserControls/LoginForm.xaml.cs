820bca23c262d4b921cd20e52dbea456fe065233
Ilya Beg <ilyabegichev@gmail.com>
2026-07-21 18:20:12 +0300

    build: Add LoginForm code-behind logic

--- WhatsAppUI/View/UserControls/LoginForm.xaml.cs
+++ WhatsAppUI/View/UserControls/LoginForm.xaml.cs
@@ -1,26 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WhatsAppUI.View.Windows;
 
 namespace WhatsAppUI.View.UserControls
 {
-    /// <summary>
-    /// Interaction logic for LoginForm.xaml
-    /// </summary>
     public partial class LoginForm : UserControl
     {
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameInfo.Text;
+            UsernameInfo.Clear();
+
+            if (false) // <- Username Authorization...
+            {
+                LoginTxt.Text = "Username already taken. Please re-enter:";
+            }
+            else
+            {
+                // display UDP/TCP choice
+                MessageBox.Show("Would you like to use UDP Communication? (No = TCP Communication)", "Protocol Choice", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.TryAgain);
+
+                // open chatting window
+                ChattingWindow chattingWindow = new();
+                chattingWindow.Show();
+                
+                // close main window
+                Window parentWindow = Window.GetWindow(this);
+                parentWindow?.Close();
+            }
         }
     }
 }
