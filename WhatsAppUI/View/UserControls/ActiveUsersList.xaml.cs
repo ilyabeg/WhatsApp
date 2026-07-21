7ea295c3c1c77aa31910f408d638c434a64a4c87
Ilya Beg <ilyabegichev@gmail.com>
2026-07-21 15:50:29 +0300

    fix: Add dynamic streches and fits for all element styles

--- WhatsAppUI/View/UserControls/ActiveUsersList.xaml.cs
+++ WhatsAppUI/View/UserControls/ActiveUsersList.xaml.cs
@@ -5,17 +5,17 @@ namespace WhatsAppUI.View.UserControls
 {
     public partial class ActiveUsersList : UserControl
     {
-        private ObservableCollection<IChatItem> _chatItems;
-        public ObservableCollection<IChatItem> ChatItems
-        {
-            get { return _chatItems; }
-            set { _chatItems = value; }
-        }
+        //private ObservableCollection<IChatItem> _chatItems;
+        //public ObservableCollection<IChatItem> ChatItems
+        //{
+        //    get { return _chatItems; }
+        //    set { _chatItems = value; }
+        //}
 
         public ActiveUsersList()
         {
-            _chatItems = new ObservableCollection<IChatItem>();
-            DataContext = this;
+            //_chatItems = new ObservableCollection<IChatItem>();
+            //DataContext = this;
             InitializeComponent();
         }
     }
